b441ce9cfb1b16c2c8e842ca87ae3751eb95882a
kBunn411 <tup81883@temple.edu>
2024-01-17 14:34:41 -0500

    completed random.c and random.h

--- print_random.c
+++ print_random.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "random.h"
 
 
 
@@ -13,7 +14,7 @@ int main()
 
 	printf("Today's random word: ");
 	for(a=0;a<7;a++)
-		putchar( randchar() );
+		putchar(randchar());
 	putchar('\n');
 
 	return(0);
